8ddc88446b9987bbb48ba156adabca51c4ea0142
GokhanCaliskan <agooqle@gmail.com>
2023-02-04 19:34:09 +0300

    sorryvercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import "./App.css";
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Layout from "./pages/Layout";
 import Projects from "./components/Projects/Projects";
 import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 function App() {
 	return (
-		<Routes>
+		<Routes history={history}>
 			<Route path="/" element={<Layout />}>
 				<Route index element={<About />}></Route>
-				<Route path="projects" element={<Projects />}></Route>
-				<Route path="blog" element={<Blog />}></Route>
-				<Route path="contact" element={<Contact />}></Route>
+				<Route path="/projects" element={<Projects />}></Route>
+				<Route path="/blog" element={<Blog />}></Route>
+				<Route path="/contact" element={<Contact />}></Route>
 			</Route>
 		</Routes>
 	);
